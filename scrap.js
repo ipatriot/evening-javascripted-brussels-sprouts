87d0861da271fc99a31603086048dc81a299ffad
ipatriots <nicolin16@gmail.com>
2015-09-13 14:01:57 -0400

    initial commit

---
+++ scrap.js
@@ -0,0 +1,10 @@
+// scrap.js
+
+var foo = "Hello my name is Jorge"
+var bar = "Jorge"
+
+console.log(foo.indexOf(bar) > - 1);
+
+
+var body = document.getElementsByTagName("body")[0];
+appendElement(body, "p", dishes[i]);
